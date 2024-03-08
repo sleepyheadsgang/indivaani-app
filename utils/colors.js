2d176316b9a33e1d7a0998cfa303113126c4aeac
Shivam Sharma <125983273+theshivam7@users.noreply.github.com>
2024-03-08 17:26:43 +0530

    Add files via upload

---
+++ utils/colors.js
@@ -0,0 +1,10 @@
+export default {
+    primary: '#000000',
+    primaryDisabled: '#e8c226',
+    lightGrey: '#dedede',
+    textColor: '#202124',
+    textColorDisabled: '#898989',
+    greyBackground: '#F2F2F7',
+    subTextColor: '#5f6368'
+
+}
